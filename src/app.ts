f405a38ebc949979a396a1f9651e8fe2f233a090
Filipe Avanzzo <fastnexi@gmail.com>
2025-08-13 21:43:36 -0300

    feat: initial api docs

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,34 @@
 import fastifyCors from "@fastify/cors";
 import Fastify from "fastify";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import { userRoutes } from "./models/user/user-route.js";
 
+import fastifySwagger from "@fastify/swagger";
+import scalarApiReference from "@scalar/fastify-api-reference";
+
 export function app() {
   const app = Fastify().withTypeProvider<ZodTypeProvider>();
+
+  if (process.env.NODE_ENV === "development") {
+    app.register(fastifySwagger, {
+      openapi: {
+        info: {
+          title: "Plataforma sobre C",
+          version: "1.0.0",
+        },
+      },
+      transform: jsonSchemaTransform,
+    });
+
+    app.register(scalarApiReference, {
+      routePrefix: "/docs",
+    });
+  }
   app.setValidatorCompiler(validatorCompiler);
   app.setSerializerCompiler(serializerCompiler);
   app.register(fastifyCors, { origin: true });
